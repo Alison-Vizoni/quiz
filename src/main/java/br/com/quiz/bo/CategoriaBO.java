925b320c219ecf5ed6eca66ca2b56bf83f770d0d
Alison-Vizoni <alison.vizoni@gmail.com>
2021-12-15 10:15:41 -0300

    Categoria não pode ser repetida

---
+++ src/main/java/br/com/quiz/bo/CategoriaBO.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.quiz.bo;
+
+import br.com.quiz.dao.CategoriaDao;
+import br.com.quiz.dao.CategoriaDaoImpl;
+import br.com.quiz.entidade.Categoria;
+import br.com.quiz.exceptions.CategoriaJaExistenteException;
+import java.util.List;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author alison
+ */
+public class CategoriaBO {
+    
+    private CategoriaDao categoriaDao;
+    private Session sessao;
+
+    public CategoriaBO() {
+        categoriaDao = new CategoriaDaoImpl();
+    }
+    
+    public Categoria salvarOuAlterar(Categoria categoria) throws CategoriaJaExistenteException{
+        List<Categoria> verificarCategoriaRepetida = categoriaDao
+                .pesquisarPorNome(categoria.getNome(), sessao);
+        
+        if(!verificarCategoriaRepetida.isEmpty()){
+            throw new CategoriaJaExistenteException();
+        }
+        
+        categoriaDao.salvarOuAlterar(categoria, sessao);
+        
+        return categoria;
+    }
+    
+}
