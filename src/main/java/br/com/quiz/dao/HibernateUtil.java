026e465ddb94af341a7199aeb627b0620caa5563
Alison-Vizoni <alison.vizoni@gmail.com>
2021-12-16 16:22:31 -0300

    Adicionando entidades

--- src/main/java/br/com/quiz/dao/HibernateUtil.java
+++ src/main/java/br/com/quiz/dao/HibernateUtil.java
@@ -5,7 +5,14 @@
  */
 package br.com.quiz.dao;
 
+import br.com.quiz.entidade.Alternativa;
+import br.com.quiz.entidade.AplicacaoQuiz;
+import br.com.quiz.entidade.AplicacaoQuizResultado;
 import br.com.quiz.entidade.Categoria;
+import br.com.quiz.entidade.Login;
+import br.com.quiz.entidade.Pergunta;
+import br.com.quiz.entidade.Quiz;
+import br.com.quiz.entidade.Usuario;
 import org.hibernate.*;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
@@ -22,6 +29,13 @@ public class HibernateUtil {
         try {
             Configuration cfg = new Configuration();
             cfg.addAnnotatedClass(Categoria.class);
+            cfg.addAnnotatedClass(Pergunta.class);
+            cfg.addAnnotatedClass(Alternativa.class);
+            cfg.addAnnotatedClass(Quiz.class);
+            cfg.addAnnotatedClass(AplicacaoQuiz.class);
+            cfg.addAnnotatedClass(AplicacaoQuizResultado.class);
+            cfg.addAnnotatedClass(Usuario.class);
+            cfg.addAnnotatedClass(Login.class);
 
             cfg.configure("/META-INF/hibernate.cfg.xml");
             StandardServiceRegistryBuilder build = new StandardServiceRegistryBuilder().applySettings(cfg.getProperties());
