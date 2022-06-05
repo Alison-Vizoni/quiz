5867482404af9817c55b7313a2102ad4680dc7f8
Bruno Rodrigues <bruno_rodrigues977@hotmail.com>
2022-06-05 15:04:30 -0300

    Adicionando validação

--- src/main/java/br/com/quiz/controle/AplicacaoQuizController.java
+++ src/main/java/br/com/quiz/controle/AplicacaoQuizController.java
@@ -41,10 +41,13 @@ public class AplicacaoQuizController implements Serializable {
     private AplicacaoQuizResultado aplicacaoQuizResultado;
     private AplicacaoQuizResultadoDao aplicacaoQuizResultadoDao;
     private AplicacaoQuiz aplicacaoQuiz;
-    private List<Pergunta> Perguntas = new ArrayList<>();
+    private List<Pergunta> perguntas = new ArrayList<>();
+    private List<Alternativa> alternativas = new ArrayList<>();
+    private List<AplicacaoQuizResultado> resultados;
     private Session sessao;
     private Pergunta pergunta;
     private Quiz quiz;
+    private boolean quizFinalizou;
     public static Long idQuiz;
 
     public AplicacaoQuizController() {
@@ -58,40 +61,141 @@ public class AplicacaoQuizController implements Serializable {
     public String validaAcessoQuiz() throws IOException {
         logger.info("entrou no validaAcessoQuiz()");
         sessao = HibernateUtil.abrirSessao();
-        String url = "";
         aplicacaoQuiz = AplicacaoQuizDao.pesquisarPorId(aplicacaoQuiz.getId(), sessao);
 
         if (aplicacaoQuiz != null) {
             if (aplicacaoQuiz.getEmails().contains(LoginController.usuarioSessao().getEmail())) {
-                url = "listaPerguntasQuiz.xhtml?faces-redirect=true";
+                if (!validaQuizInicio(aplicacaoQuiz)) {
+                    return "listaPerguntasQuiz.xhtml?faces-redirect=true";
+                }
+                aplicacaoQuiz = new AplicacaoQuiz();
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Acesso negado", "Você já respondeu esse quiz"));
             } else {
+                aplicacaoQuiz = new AplicacaoQuiz();
                 FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Acesso negado", "Não possui acesso"));
             }
         } else {
+            aplicacaoQuiz = new AplicacaoQuiz();
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Quiz não encontrado", "Nenhum quiz com esse código foi encontrado"));
         }
-        return url;
+        return "";
     }
 
     public void pesquisaQuiz() {
         logger.info("entrou no pesquisaQuiz()");
         sessao = HibernateUtil.abrirSessao();
+        verificaPerguntasFinalizadas();
+        quizFinalizou = verificaPerguntaFinalizado();
+    }
 
-        aplicacaoQuiz = AplicacaoQuizDao.pesquisarPorId(aplicacaoQuiz.getId(), sessao);
+    public void verificaPerguntasFinalizadas() {
+        logger.info("entrou no verificaPerguntasFinalizadas()");
+        perguntas = aplicacaoQuiz.getQuiz().getPerguntas();
+        aplicacaoQuizResultadoDao = new AplicacaoQuizResultadoDaoImpl();
+        resultados = aplicacaoQuizResultadoDao.pesquisarPorId(aplicacaoQuiz.getId(), LoginController.usuarioSessao().getId(), sessao);
+
+        for (AplicacaoQuizResultado resultado : resultados) {
+            for (int i = 0; i < perguntas.size(); i++) {
+                if (perguntas.get(i).getId() == resultado.getAlternativa().getPergunta().getId()) {
+                    perguntas.get(i).setStatusAtivo(false);
+                }
+            }
+
+        }
+    }
+
+    public boolean validaPerguntaResponder(int idPergunta) {
+        for (AplicacaoQuizResultado resultado : resultados) {
+            if (idPergunta == resultado.getAlternativa().getPergunta().getId()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String finalizarQuiz() {
+        logger.info("entrou no finalizarQuiz()");
+        aplicacaoQuizResultadoDao = new AplicacaoQuizResultadoDaoImpl();
+        removerPerguntasJaSalvadas();
+
+        for (int i = 0; i < perguntas.size(); i++) {
+            sessao = HibernateUtil.abrirSessao();
+            aplicacaoQuizResultadoDao = new AplicacaoQuizResultadoDaoImpl();
+            aplicacaoQuizResultado = new AplicacaoQuizResultado(Date.from(Instant.now()));
+            aplicacaoQuizResultado.setUsuario(LoginController.usuarioSessao());
+            aplicacaoQuizResultado.setAplicacaoQuiz(aplicacaoQuiz);
+            aplicacaoQuizResultado.setAlternativa(salvaAlternativaErrada(perguntas.get(i).getAlternativas()));
+            sessao = HibernateUtil.abrirSessao();
+            aplicacaoQuizResultadoDao.salvarOuAlterar(aplicacaoQuizResultado, sessao);
+            sessao.close();
+        }
+
+        aplicacaoQuiz = new AplicacaoQuiz();
+        return "inicioResponderQuiz";
+    }
+
+    ;
+    
+    public void removerPerguntasJaSalvadas() {
+
+        for (int i = 0; i < perguntas.size(); i++) {
+            for (AplicacaoQuizResultado resultado : resultados) {
+                if (perguntas.get(i).getId() == resultado.getAlternativa().getPergunta().getId()) {
+                    perguntas.remove(i);
+
+                }
+            }
+        }
 
     }
 
-    public void responderPergunta(int idPergunta) {
-        logger.info("entrou no responderPergunta()");
-        List<Pergunta> perguntas = aplicacaoQuiz.getQuiz().getPerguntas();
+    public Alternativa salvaAlternativaErrada(List<Alternativa> alternativas) {
 
-        for (int i = 0; i < perguntas.size(); i++) {
-            if (perguntas.get(i).getId() == idPergunta) {
-                pergunta = perguntas.get(i);
+        Alternativa alternativaErrada = new Alternativa();
+        for (int i = 0; i < alternativas.size(); i++) {
+            if (!alternativas.get(i).isStatusCorreta()) {
+                alternativaErrada = alternativas.get(i);
                 break;
             }
         }
-        System.out.println(pergunta);
+
+        return alternativaErrada;
+    }
+
+    public boolean verificaPerguntaFinalizado() {
+        logger.info("entrou no verificaPerguntaFinalizado()");
+
+        perguntas = aplicacaoQuiz.getQuiz().getPerguntas();
+        boolean foiFinalizado = true;
+        for (int i = 0; i < perguntas.size(); i++) {
+            if (perguntas.get(i).getStatusAtivo()) {
+                foiFinalizado = false;
+                break;
+
+            };
+        }
+        return foiFinalizado;
+    }
+
+    ;
+    public String responderPergunta(int idPergunta) {
+        logger.info("entrou no responderPergunta()");
+
+        if (validaPerguntaResponder(idPergunta)) {
+            FacesContext
+                    .getCurrentInstance()
+                    .addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Pergunta já foi respondida", "Está pergunta já foi respondida"));
+            return "";
+        } else {
+            for (int i = 0; i < perguntas.size(); i++) {
+                if (perguntas.get(i).getId() == idPergunta) {
+                    pergunta = perguntas.get(i);
+                    break;
+                }
+            }
+            return "responderQuiz";
+        }
+
     }
 
     public String enviarResposta(Alternativa alternativa) {
@@ -100,7 +204,7 @@ public class AplicacaoQuizController implements Serializable {
         aplicacaoQuizResultado.setUsuario(LoginController.usuarioSessao());
         aplicacaoQuizResultado.setAplicacaoQuiz(aplicacaoQuiz);
         aplicacaoQuizResultado.setAlternativa(alternativa);
-        
+
         sessao = HibernateUtil.abrirSessao();
         aplicacaoQuizResultadoDao.salvarOuAlterar(aplicacaoQuizResultado, sessao);
         sessao.close();
@@ -117,6 +221,14 @@ public class AplicacaoQuizController implements Serializable {
             return "#ba87e9";
         }
         return "#e98787";
+    }
+
+    private boolean validaQuizInicio(AplicacaoQuiz aplicacao) {
+        aplicacaoQuizResultadoDao = new AplicacaoQuizResultadoDaoImpl();
+        sessao = HibernateUtil.abrirSessao();
+        List<AplicacaoQuizResultado> aplicacaoQuizResultado = aplicacaoQuizResultadoDao.pesquisarPorId(aplicacao.getId(), LoginController.usuarioSessao().getId(), sessao);
+        sessao.close();
+        return aplicacaoQuizResultado.size() == aplicacao.getQuiz().getPerguntas().size();
     }
 
 //	
@@ -142,6 +254,22 @@ public class AplicacaoQuizController implements Serializable {
 
     public void setPergunta(Pergunta pergunta) {
         this.pergunta = pergunta;
+    }
+
+    public List<Pergunta> getPerguntas() {
+        return perguntas;
+    }
+
+    public void setPerguntas(List<Pergunta> Perguntas) {
+        this.perguntas = Perguntas;
+    }
+
+    public boolean isQuizFinalizou() {
+        return quizFinalizou;
+    }
+
+    public void setQuizFinalizou(boolean quizFinalizou) {
+        this.quizFinalizou = quizFinalizou;
     }
 
 }
